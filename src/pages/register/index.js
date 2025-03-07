dc131ff131463e593b598ed95254396097986290
LeoPro23 <lguimaray@unitru.edu.pe>
2025-03-07 14:41:04 -0500

    style: login y register sin github ni google

--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -259,9 +259,10 @@ export default function SignUp() {
             </div>
           </form>
 
-          {/* Botón para iniciar sesión con GitHub */}
+          {/* Botón para iniciar sesión con GitHub o Google*/}
+          {/*
           <SignSocial />
-
+          */}
 
           <p className="mt-10 text-sm text-center leading-6 text-gray-500 dark:text-gray-400">
             ¿Tienes una cuenta?{' '}
