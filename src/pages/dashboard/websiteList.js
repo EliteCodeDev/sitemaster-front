d8a51bb40e3022d2930e52733748c9782acf2fc5
ivanfernandeze <ivanfer105esq@gmail.com>
2025-03-09 12:31:44 -0500

    listo

--- src/pages/dashboard/websiteList.js
+++ src/pages/dashboard/websiteList.js
@@ -1,104 +1,3 @@
-{/* <FetchWebsites />
-import useSWR from "swr"
-import { useSession } from "next-auth/react"
-import WebsiteCard from "./websiteCard"
-import OrderSkeleton from "../../components/loaders/OrderSkeleton"
-import Link from "next/link"
-import { PlusIcon } from "@heroicons/react/24/solid"
-
-import { strapiUrl } from "@/routes/routes"
-import Cbutton from "@/components/interfaz/Cbutton"
-
-const fetcher = async (url, jwt) => {
-  try {
-    const response = await fetch(url, {
-      headers: {
-        Authorization: `Bearer ${jwt}`,
-      },
-    })
-
-    if (!response.ok) {
-      throw new Error(`❌ Error en la solicitud: ${response.statusText}`)
-    }
-
-    return await response.json()
-  } catch (error) {
-    console.error("❌ Error al obtener datos de Strapi:", error)
-    throw error
-  }
-}
-
-const FetchWebsites = () => {
-  const { data: session } = useSession()
-  const jwt = session?.jwt
-
-  const { data, error, isLoading } = useSWR(jwt ? `${strapiUrl}/api/users/me?populate=websites` : null, (url) =>
-    fetcher(url, jwt),
-  )
-
-  if (isLoading) {
-    return <OrderSkeleton />
-  }
-
-  if (error) {
-    return <p className="text-red-500">Error: {error.message}</p>
-  }
-
-  if (!data) return <OrderSkeleton />
-
-  if (!data?.websites?.length) {
-    return (
-      <>
-        <div className="bg-white px-6 py-10 text-center w-full mx-auto min-h-[400px] flex flex-col justify-center items-center card-border">
-          <svg
-            className="mx-auto h-12 w-12 text-gray-400"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            aria-hidden="true"
-          >
-            <path
-              vectorEffect="non-scaling-stroke"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M9 13h6m-3-3v6m-9 1V7a2 2 0 012-2h6l2 2h6a2 2 0 012 2v8a2 2 0 01-2 2H5a2 2 0 01-2-2z"
-            />
-          </svg>
-          <h3 className="mt-4 text-lg font-semibold text-gray-800">¡Suscribete y crea tu primer website en sitemaster!</h3>
-          da
-          <p className="mt-2 text-sm text-gray-600">Comience contratando uno de nuestros planes.</p>
-          <Link href="/upgrade/" className="mt-5">
-            <Cbutton classType={"primary"} startContent={<PlusIcon className="w-5" />}>
-              <span>Crea un website</span>
-            </Cbutton>
-          </Link>
-        </div>
-      </>
-    )
-  }
-
-  return (
-    <div className="grid grid-cols-1 gap-4">
-      {data.websites.map((website, index) => (
-        <WebsiteCard
-          key={index}
-          websiteId={website.websiteId}
-          websiteName={website.websiteName}
-          isActive={website.isActive}
-          endDate={website.endDate}
-          serverUrl={website.server_url}
-        />
-      ))}
-    </div>
-  )
-}
-
-export default FetchWebsites
-
-
-*/}
-
 import useSWR from 'swr';
 import { useSession } from 'next-auth/react';
 import WebsiteCard from './websiteCard';
@@ -214,9 +113,7 @@ const FetchWebsites = () => {
             {sub.websites?.length > 0 ? (
               <div className="grid grid-cols-1 gap-4 mt-4">
                 {sub.websites.map((website, idx) => {
-                  // Validamos si status_woo es exactamente "active"
                   const statusWoo = sub.status_woo === "active";
-
                   return (
                     <WebsiteCard
                       key={idx}
